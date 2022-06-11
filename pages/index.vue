829f23fb34604f72eacffbf130a3de8143e5b229
Bavo <61050421+Ovab@users.noreply.github.com>
2022-06-12 01:32:49 +0200

    Nuxt project restart

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center items-center h-max bg-black">
+    <h1 class="text-white">This site is under construction and needs to be built</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>

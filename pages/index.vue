da26d8c2dc4b88540f592b59a69c0b15461a6c4a
Bavo <61050421+Ovab@users.noreply.github.com>
2022-07-13 09:20:09 +0200

    Begin

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="flex justify-center items-center h-max bg-black">
-    <h1 class="text-white">This site is under construction and needs to be built</h1>
+  <div class="flex flex-col justify-center items-center h-max bg-black">
+    <h1 class="text-white text-9xl mb-10">Bavo Knol</h1>
+    <div class="text-white text-6xl mb-10">
+      <h2>Full stack developer</h2>
+    </div>
+  </div>
+  <div class="flex flex-col items-center h-max bg-gray-800 py-20 -mt-px">
+    <div class="flex gap-5 flex-col flex-col">
+      <h3 class="text-white text-5xl">Professional</h3>
+      <h3 class="text-white text-5xl ml-5">Development</h3>
+      <h3 class="text-white text-5xl">Experience</h3>
+    </div>
+
+    <ul class="text-white text-3xl mt-10 list-disc">
+      <li>Jaspers Media</li>
+    </ul>
+    <p class="text-white mt-2">I'm a part-time Full stack developer here.<br>
+                               After completing my internship lasting 7 months<br>
+                               I was offered a job to work there under a 0-hour contract<br>
+    </p>
   </div>
 </template>
 

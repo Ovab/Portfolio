180cdb552c02ec31123de93c25e3916559103848
Bavo <61050421+Ovab@users.noreply.github.com>
2022-08-14 13:39:18 +0200

    Nicer way to add projects

---
+++ components/LanguageTag.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="tag"
+       class="p-1 drop-shadow-2xl rounded w-fit text-gray-800 font-medium"
+      :style="{'background-color': tagColor}">
+    {{tag}}
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "LanguageTag",
+
+  props:{
+    tag: {
+      type: String,
+      required: true
+    },
+    tagColor: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
